18da69ca72d1437997268c30eb9b3c9ef1f0ead3
rifaldiusn <148640937+rifaldiusn@users.noreply.github.com>
2024-12-06 08:10:21 +0000

    commit

--- Tubes/HeadTubes.h
+++ Tubes/HeadTubes.h
@@ -1,6 +1,53 @@
 #ifndef HEADTUBES_H_INCLUDED
 #define HEADTUBES_H_INCLUDED
+#include <iostream>
+using namespace std; 
 
+typedef struct Pelanggan *adrPelanggan;
+typedef struct Pesanan *adrPesanan;
+typedef struct MenuMakanan *adrMenuMakanan;
+typedef struct MenuMinuman *adrMenuMinuman;
+
+struct antrian
+{
+    adrPelanggan headPelanggan;
+    adrPelanggan tailPelanggan;
+};
+
+struct Pesanan
+{
+    string namaPesanan;
+    int harga;
+    adrPesanan nextPesanan;
+};
+
+struct pelanggan
+{
+    string nama;
+    int totalHarga; 
+    adrPelanggan nextPelanggan;
+    adrPesanan firstPesanan;
+};
+
+struct MenuMakanan
+{
+    string namaMenu;
+    int harga;
+    adrMenuMakanan nextMakanan;
+};
+
+struct MenuMinuman
+{
+    string namaMenu;
+    int harga;
+    adrMenuMinuman nextMinuman;
+};
+
+struct Menu
+{
+    adrMenuMakanan firstMakanan;
+    adrMenuMinuman firstMinuman;
+};
 
 
 #endif // HEADTUBES_H_INCLUDED
